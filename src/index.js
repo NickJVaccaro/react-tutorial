e89bdc5ba5e2abf5683841470d5ebb3eb1bee981
NickJVaccaro <njvaccaro0@gmail.com>
2019-04-30 15:40:47 -0400

    Construct board with for loops
    The board used to be constructed by hardcoded values. They have been
    removed and replaced with a for loop, so expanding the board is much
    easier now.

--- src/index.js
+++ src/index.js
@@ -22,30 +22,31 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square 
+        key={i}
         value={ this.props.squares[i] }
         onClick={ () => this.props.onClick(i) }
       />
     )
   }
 
+  createBoardSquares() {
+    let table = [];
+
+    for(let i = 0; i < 3; i++) {
+      let children = [];
+      for(let j = 0; j < 3; j++) {
+        children.push(this.renderSquare((i * 3) + j));
+      }
+      table.push(<div className="board-row">{children}</div>);
+    }
+
+    return table;
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createBoardSquares()}
       </div>
     );
   }
